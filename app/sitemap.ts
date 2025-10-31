d3f5b2f43caf935b1a77b634fa76a20b5edad5b9
jaemjeon <piepie1346@gmail.com>
2025-10-31 16:03:13 +0900

    remove sitemap

--- app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from 'next'
-import { generateSitemap } from './generateMetadata'
-
-export const dynamic = 'force-static'
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return generateSitemap()
-} 
